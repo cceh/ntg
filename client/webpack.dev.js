bb7b5e99037ad673235f066e2d906d1ba341363e
Marcello Perathoner <marcello@perathoner.de>
2020-05-05 14:52:38 +0200

    disable js bundle analyzer

--- client/webpack.dev.js
+++ client/webpack.dev.js
@@ -6,6 +6,6 @@ module.exports = merge (common, {
     mode: 'development',
     devtool: 'eval-source-map',
     plugins: [
-        new BundleAnalyzer (),
+        // new BundleAnalyzer (),
     ],
 });
